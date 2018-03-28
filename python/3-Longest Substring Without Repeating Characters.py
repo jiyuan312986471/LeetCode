ba40b236ca2161231662fff2ca9f34d3c7cf8a8a
JI Yuan <ji.yuan@matmut.fr>
2018-03-28 10:47:53 +0200

    python: Problem 3

--- python/3-Longest Substring Without Repeating Characters.py
+++ python/3-Longest Substring Without Repeating Characters.py
@@ -4,3 +4,13 @@ class Solution:
         :type s: str
         :rtype: int
         """
+        lengs = [0]
+        for i, c in enumerate(s):
+            ss = c
+            for ch in s[i + 1:]:
+                if ch not in ss:
+                    ss += ch
+                else:
+                    break
+            lengs.append(len(ss))
+        return max(lengs)
