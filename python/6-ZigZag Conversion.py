a8664f29a24fc9653e3d66849c3b6190cc24a990
JI Yuan <ji.yuan@matmut.fr>
2018-04-04 17:55:07 +0200

    python: Problem 6: Accepted solution

--- python/6-ZigZag Conversion.py
+++ python/6-ZigZag Conversion.py
@@ -5,3 +5,18 @@ class Solution:
         :type numRows: int
         :rtype: str
         """
+        if numRows < 2:
+            return s
+
+        # num_char in one period
+        half_T = numRows - 1
+        T = 2 * half_T
+
+        rows = [''] * numRows
+        for i, c in enumerate(s):
+            n = i % T
+            if n <= half_T:
+                rows[n] += c
+            else:
+                rows[T - n] += c
+        return ''.join(rows)
