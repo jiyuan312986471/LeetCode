3b0951f82e74b1ec7f77b45d2e40a19a665a119c
JI Yuan <ji.yuan@matmut.fr>
2018-04-05 11:24:09 +0200

    python: Problem 7: Accepted Solution

--- python/7-Reverse Integer.py
+++ python/7-Reverse Integer.py
@@ -4,3 +4,10 @@ class Solution:
         :type x: int
         :rtype: int
         """
+        s = '-' if x < 0 else ''
+        x = str(abs(x))
+        s += x[::-1]
+        s = int(s)
+        if s > 2 ** 31 - 1 or s < -2 ** 31:
+            return 0
+        return s
