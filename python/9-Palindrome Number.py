84c19a726d62fae5f6cd1c0f2200284479a8f864
JI Yuan <ji.yuan@matmut.fr>
2018-04-05 17:01:35 +0200

    python: Problem 9: Accepted solution

--- python/9-Palindrome Number.py
+++ python/9-Palindrome Number.py
@@ -4,3 +4,12 @@ class Solution:
         :type x: int
         :rtype: bool
         """
+        if x < 0:
+            return False
+        elif x > 0 and x % 10 == 0:
+            return False
+        rev = 0
+        while x > rev:
+            rev = rev * 10 + x % 10
+            x = x // 10
+        return x == rev or x == rev // 10
