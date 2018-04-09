6b75f498e85e264dd66db041927f9bdd9ec377ac
JI Yuan <ji.yuan@matmut.fr>
2018-04-09 11:59:02 +0200

    python: Problem 10: Accepted Solution

--- python/10-Regular Expression Matching.py
+++ python/10-Regular Expression Matching.py
@@ -5,3 +5,19 @@ class Solution:
         :type p: str
         :rtype: bool
         """
+        dp = [[True] + [False] * len(s)]
+        for i, pc in enumerate(p):
+            r = [pc == '*' and dp[-2][0]]
+            for j, sc in enumerate(s):
+                j += 1  # counting col for empty s
+                if pc == '*':
+                    match_0 = dp[-2][j]
+                    match_1_or_plus = (p[i - 1] == sc or p[i - 1] == '.') and r[
+                        j - 1]
+                    r.append(match_0 or match_1_or_plus)
+                elif pc == '.':
+                    r.append(dp[-1][j - 1])
+                else:
+                    r.append(dp[-1][j - 1] and pc == sc)
+            dp.append(r)
+        return dp[-1][-1]
