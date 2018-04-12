7dba2d07172d0c14e2f26c8c58bb3fba939aaf27
JI Yuan <ji.yuan@matmut.fr>
2018-04-12 11:26:24 +0200

    python: Problem 12: Accepted Solution

--- python/12-Integer to Roman.py
+++ python/12-Integer to Roman.py
@@ -4,3 +4,12 @@ class Solution:
         :type num: int
         :rtype: str
         """
+        M = ['', 'M', 'MM', 'MMM']
+        C = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+        X = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+        I = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+
+        m, num = divmod(num, 1000)
+        c, num = divmod(num, 100)
+        x, num = divmod(num, 10)
+        return M[m] + C[c] + X[x] + I[num]
