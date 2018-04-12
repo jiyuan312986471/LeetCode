18bf55045d9f010344ed7c7f571048c7a2d205fa
JI Yuan <ji.yuan@matmut.fr>
2018-04-12 10:50:29 +0200

    python: Problem 11: Accepted Solution

--- python/11-Container With Most Water.py
+++ python/11-Container With Most Water.py
@@ -4,3 +4,16 @@ class Solution:
         :type height: List[int]
         :rtype: int
         """
+        max_area = 0
+        i, j = 0, len(height) - 1
+        while i < j:
+            area = (j - i) * min(height[i], height[j])
+            if area > max_area:
+                max_area = area
+
+            if height[i] <= height[j]:
+                i += 1
+            else:
+                j -= 1
+
+        return max_area
