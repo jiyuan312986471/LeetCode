15ff34b74b792a9de6d0659be6af12d7455372c0
JI Yuan <ji.yuan@matmut.fr>
2018-04-12 15:51:15 +0200

    python: Problem 15: Accepted Solution

--- python/15-3Sum.py
+++ python/15-3Sum.py
@@ -4,3 +4,25 @@ class Solution:
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        res = []
+        nums.sort()
+        leng = len(nums)
+        for i in range(leng - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l, r = i + 1, leng - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return res
