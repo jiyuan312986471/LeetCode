1f5cc6427e13e8569e3636ded3a86080bc50c93b
JI Yuan <ji.yuan@matmut.fr>
2018-04-12 14:25:23 +0200

    python: Problem 14: Accepted Solution

--- python/14-Longest Common Prefix.py
+++ python/14-Longest Common Prefix.py
@@ -4,3 +4,9 @@ class Solution:
         :type strs: List[str]
         :rtype: str
         """
+        if not strs:
+            return ''
+        for i, chs in enumerate(zip(*strs)):
+            if len(set(chs)) > 1:
+                return strs[0][:i]
+        return min(strs)
