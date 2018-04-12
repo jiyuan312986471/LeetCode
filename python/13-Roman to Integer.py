d629df86616c2e72a2e07f7e4165107dfaf9fb30
JI Yuan <ji.yuan@matmut.fr>
2018-04-12 12:01:44 +0200

    python: Problem 13: Accepted Solution

--- python/13-Roman to Integer.py
+++ python/13-Roman to Integer.py
@@ -4,3 +4,21 @@ class Solution:
         :type s: str
         :rtype: int
         """
+        if not s:
+            return 0
+        ch_v = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        res = ch_v[s[-1]]
+        for i in range(len(s) - 2, -1, -1):
+            if ch_v[s[i]] < ch_v[s[i + 1]]:
+                res -= ch_v[s[i]]
+            else:
+                res += ch_v[s[i]]
+        return res
