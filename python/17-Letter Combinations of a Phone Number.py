a0641e34c7c1a85a746a1b69a61f2f12e26915e4
JI Yuan <ji.yuan@matmut.fr>
2018-06-05 17:31:50 +0200

    python: Problem 17: Accepted Solution

--- python/17-Letter Combinations of a Phone Number.py
+++ python/17-Letter Combinations of a Phone Number.py
@@ -4,3 +4,17 @@ class Solution:
         :type digits: str
         :rtype: List[str]
         """
+        if not digits:
+            return []
+        d_ch = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        return [''.join(x) for x in
+                itertools.product(*[d_ch[c] for c in digits])]
